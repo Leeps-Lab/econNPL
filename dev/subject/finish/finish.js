292be8163d1444826b0e936979de21c9aa22d17e
Joshua Pena <penacalata@gmail.com>
2015-12-16 18:39:28 -0800

    finished inital changes to decrete matrix. on to config settings.

---
+++ dev/subject/finish/finish.js
@@ -0,0 +1,8 @@
+Redwood.controller("SubjectCtrl", ["$rootScope", "$scope", "RedwoodSubject", function($rootScope, $scope, rs) {
+
+  rs.on_load(function() {
+		$scope.pointsByPeriod = rs.subject[rs.user_id].points_by_period();
+    $scope.pointsByPeriod.shift();
+	});
+
+}]);
---
+++ dev/subject/start/start.js
@@ -0,0 +1,119 @@
+Redwood.controller("SubjectCtrl", ["$rootScope", "$scope", "RedwoodSubject", function($rootScope, $scope, rs) {
+
+  $scope.messages = [];
+  $scope.count = 0;
+
+  rs.on_load(function() {
+		rs.config.pairs.forEach(function(pair, index) { //decide who is the first player and who is the second player
+			var userIndex = pair.indexOf(parseInt(rs.user_id));
+			if(userIndex > -1) {
+				$scope.pair_index = index;
+				$scope.user_index = userIndex;
+				$scope.partner_id = pair[($scope.user_index + 1) % 2].toString();
+			}
+		});
+
+		$scope.matrix = $scope.user_index === 0 ? rs.config.matrix : transpose(rs.config.matrix);
+
+		$scope.round = 0;
+
+		rs.synchronizationBarrier('on_load').then(function() {
+			rs.trigger("next_round"); //Start first round
+		});
+	});
+
+	$scope.onSelection = function(selection) {
+		if($scope.inputsEnabled) {
+			$scope.selection = selection;
+		}
+	};
+
+	$scope.confirm = function() {
+		if(!angular.isNullOrUndefined($scope.selection)) { //Check that user has selected an action
+			$scope.inputsEnabled = false;
+			rs.trigger("action", $scope.selection);
+		} else {
+			alert("Please select an action.");
+		}
+	};
+
+  $scope.sendMessage = function(e) {
+    if (e && e.keyCode !== 13) return;
+    if ($scope.message === "") return;
+    rs.trigger("sendmessage", {
+      from: $scope.user_index,
+      text: $scope.message
+    });
+    $scope.message = "";
+  };
+
+  // on messages
+	rs.on("action", function(value) {
+		$scope.inputsEnabled = false;
+		$scope.selection = value;
+		$scope.action = value;
+		rs.synchronizationBarrier('round_' + $scope.round, [$scope.partner_id]).then(function() { //Call this function once the partner subject has reached this point
+			allocateRewards($scope.action, $scope.partnerAction);
+			rs.trigger("next_round");
+		}, [$scope.partner_id]);
+	});
+  rs.on("sendmessage", function(message) {
+    $scope.messages.push(message);
+    console.log($scope.count++);
+  })
+
+  // recv messages
+	rs.recv("action", function(sender, value) {
+		if(sender == $scope.partner_id) {
+			$scope.partnerAction = value;
+		}
+	});
+  rs.recv("sendmessage", function(sender, message) {
+    if (sender == $scope.partner_id) {
+      $scope.messages.push(message);
+      console.log('recv');
+    }
+  });
+
+	rs.on("next_round", function() {
+		$scope.round++;
+		$scope.rounds = $.isArray(rs.config.rounds) ? rs.config.rounds[$scope.pair_index] : rs.config.rounds;
+
+		$scope.prevAction = $scope.action;
+		$scope.prevPartnerAction = $scope.partnerAction;
+
+		if($scope.round > $scope.rounds) {
+			rs.next_period(5);
+		} else {
+			$scope.inputsEnabled = true;
+		}
+	});
+
+	var allocateRewards = function(ai, aj){
+		$scope.reward = $scope.matrix[ai - 1][aj - 1][0];
+		rs.add_points($scope.reward);
+
+		$scope.partnerReward = $scope.matrix[ai - 1][aj - 1][1];
+	};
+
+	var transpose = function(matrix) { //transpose a 2x2 matrix
+		var transposed = [[[], []], [[], []]];
+		for(var i = 0; i < 4; i++){
+			var row = Math.floor(i/2);
+			var column = i % 2;
+			transposed[column][row] = [matrix[row][column][1], matrix[row][column][0]];
+		}
+		return transposed;
+	};
+
+}]);
+
+Redwood.filter("action", function() {
+	var actions = {
+		1: "A",
+		2: "B"
+	};
+	return function(value) {
+		return	actions[value];
+	};
+});
